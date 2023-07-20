b6e8a7372c7b690c8012c90917b2d21ddc6906c9
Sebastian Döll <sebastian@katallaxie.me>
2023-07-20 04:23:03 +0000

    chore: add checksize target

---
+++ packages/fragment/rollup.config.js
@@ -0,0 +1,32 @@
+import summary from 'rollup-plugin-summary'
+import { terser } from 'rollup-plugin-terser'
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+
+export default {
+  input: 'dist/src/app-fragment.js',
+  output: {
+    file: 'app-fragment.bundled.js',
+    format: 'esm',
+  },
+  onwarn(warning) {
+    if (warning.code !== 'THIS_IS_UNDEFINED') {
+      console.error(`(!) ${warning.message}`)
+    }
+  },
+  plugins: [
+    replace({ 'Reflect.decorate': 'undefined' }),
+    resolve(),
+    terser({
+      ecma: 2017,
+      module: true,
+      warnings: true,
+      mangle: {
+        properties: {
+          regex: /^__/,
+        },
+      },
+    }),
+    summary(),
+  ],
+}
